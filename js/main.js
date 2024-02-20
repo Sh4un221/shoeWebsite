7b2028e85d1df39e5b4f918aaf45abea21d769f6
Sh4un221 <127805282+Sh4un221@users.noreply.github.com>
2024-02-20 12:09:59 -0500

    feat: :sparkles: The page has been finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 import {} from "./module/facturacion.js"
+const menu =document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click",() => {
+    menu.classList.toggle('nav-toggle');
+});
